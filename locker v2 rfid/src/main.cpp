c76a451521f0f3a8e99a1e01d48fedf487311236
unknown <dikilmj@gmail.com>
2026-01-21 00:31:35 +0700

    test

--- locker v2 rfid/src/main.cpp
+++ locker v2 rfid/src/main.cpp
@@ -60,20 +60,20 @@ void init_mypin()
 	led.mode = acc_mode::mode_fastloop2X;
 	SPI.begin();
 
-	// if (!nfc.begin() || !nfc.SAMConfig())
-	// {
-	// 	if (rfid.enable_debug)
-	// 	{
-	// 		Serial.println("::NFC not already");
-	// 	}
-	// }
-	// else
-	// {
-	// 	if (rfid.enable_debug)
-	// 	{
-	// 		Serial.println("NFC ALREADY");
-	// 	}
-	// }
+	if (!nfc.begin() || !nfc.SAMConfig())
+	{
+		if (rfid.enable_debug)
+		{
+			Serial.println("::NFC not already");
+		}
+	}
+	else
+	{
+		if (rfid.enable_debug)
+		{
+			Serial.println("NFC ALREADY");
+		}
+	}
 	pinMode(led.pin, OUTPUT);
 	pinMode(buzzer.pin, OUTPUT);
 }
