f68fce449ee67fc783d7bcb9830b0edd2154b6fe
unknown <dikilmj@gmail.com>
2026-01-23 20:06:38 +0700

    sync_

--- locker v2 rfid/include/commond.h
+++ locker v2 rfid/include/commond.h
@@ -130,7 +130,7 @@ struct ethernet_state
     // char server[50] = "192.168.100.9";
     int port = 8000;
     // int port = 5000;
-    String endpoint = "/get-data.json";
+    String endpoint = "/set-data.json";
     bool initialized = false;
     bool get_new_data = false, last_get_data = false;
     unsigned long timeout_start = 0;
