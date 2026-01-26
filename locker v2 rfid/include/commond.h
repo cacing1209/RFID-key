e8ae6e036235e9b112b89c755d208842914bbcf3
unknown <dikilmj@gmail.com>
2026-01-26 15:58:57 +0700

    sync_

--- locker v2 rfid/include/commond.h
+++ locker v2 rfid/include/commond.h
@@ -6,7 +6,7 @@
 #include <../lib/Ethernet-2.0.2/src/Ethernet.h>
 
 #define sizeRelay 32
-const int pin_IO[sizeRelay] = {9, 23, 24, 25, 26, 27,
+const int pin_IO[sizeRelay] = {22, 23, 24, 25, 26, 27,
                                28, 29, 30, 31, 32, 33,
                                34, 35, 36, 37, 38, 39,
                                40, 41, 42, 43, 44, 45,
