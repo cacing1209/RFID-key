5d401deda7102a9b33e377bf017c97914c5cc2ac
unknown <dikilmj@gmail.com>
2026-01-26 16:20:46 +0700

    sync_

--- locker v2 rfid/src/asset/memory.cpp
+++ locker v2 rfid/src/asset/memory.cpp
@@ -75,7 +75,7 @@ bool storage_state::save_data(database_s *db, database_s *new_db)
         {
             if (enable_debug)
                 Serial.println(":mem:card replace=>" + String(i));
-            // epr.put(addr, db[i]);
+            epr.put(addr, db[i]);
         }
         addr += sizeof(database_s);
 
