8fde6c8ca0b170150c6fa83d9d2b998d6c372525
cacing1209 <dikilmj@gmail.com>
2026-03-04 18:41:29 +0700

    update-minor

--- locker v2 rfid/src/asset/main_ethernet_.cpp
+++ locker v2 rfid/src/asset/main_ethernet_.cpp
@@ -823,7 +823,8 @@ void ethernet_state::send_eventLog(const unsigned long uid_decimal, byte index)
 {
     EthernetClient logClient;
 
-    server_log = "93.144.178.187";
+    // server_log = "93.144.178.187";
+    server_log = "93.144.178.53";
     portServer_log = 3000;
 
     if (!logClient.connect(server_log, portServer_log))
