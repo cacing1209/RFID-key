cc8021214c222431212bb167d854e3455c5a4f8c
cacing1209 <dikilmj@gmail.com>
2026-03-04 16:47:35 +0700

    update-minor

--- locker v2 rfid/src/asset/main_ethernet_.cpp
+++ locker v2 rfid/src/asset/main_ethernet_.cpp
@@ -198,7 +198,7 @@ void ethernet_state::loop(database_s *db, storage_state *memory, Relay_state *lo
             return;
         }
     }
-    ntpCfg.update(60000);
+    ntpCfg.update(10000);
     EthernetClient client = server.available();
     if (client)
     {
