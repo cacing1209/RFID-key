e39367b93a55607b427c237036badeb0f620fb3d
diki lmj <dikilmj@gmail.com>
2026-05-05 15:38:19 +0700

    update

--- src/asset/main_ethernet_.cpp
+++ src/asset/main_ethernet_.cpp
@@ -928,7 +928,7 @@ void ethernet_state::send_eventLog(const unsigned long uid_decimal, byte index)
 {
     EthernetClient logClient;
 
-    server_log = "http://locker-logs.qyubit.io/";
+    server_log = "192.168.0.102";
     portServer_log = 3000;
     String key = token_sck_log;
 
