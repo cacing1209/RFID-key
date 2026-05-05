739191b3d3e5e02cb39e54e56816eaf0dc9009cb
diki lmj <dikilmj@gmail.com>
2026-05-05 13:43:38 +0700

    update

--- src/asset/main_ethernet_.cpp
+++ src/asset/main_ethernet_.cpp
@@ -569,9 +569,12 @@ void ethernet_state::handle_get_data(EthernetClient &client, database_s *db)
     if (!db)
     {
         send_error(client, 500, "Database not available");
+        #ifdef DEBUG_ETH
+            Serial.println(":eth:database not ready");
+        #endif
         return;
     }
-
+    
 #ifdef DEBUG_ETH
     Serial.println(":eth:get all data");
 #endif
