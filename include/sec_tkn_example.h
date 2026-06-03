8263433c7324deeb1e1409d7bd54d15a4579c518
diki lmj <dikilmj@gmail.com>
2026-06-03 22:36:58 +0700

    resolve example token

--- include/sec_tkn_example.h
+++
@@ -1,7 +0,0 @@
-#ifndef sec_tkn_H
-#define sec_tkn_H
-
-#define token_sck_client "token_lu kasih di sini ya njing hehee"
-#define token_sck_log "token_lu kasih di sini ya njing hehee"
-
-#endif
---
+++ include/sec_tkn_examplee.h
@@ -0,0 +1,7 @@
+#ifndef sec_tkn_H
+#define sec_tkn_H
+
+#define token_sck_client "tokenclient"
+#define token_sck_log "tokenlog"
+
+#endif
