4d05d66203b0144946267c3e8de4962e1245cf9f
diki lmj <dikilmj@gmail.com>
2026-06-22 18:14:52 +0700

    rename controller device

--- include/commond.h
+++ include/commond.h
@@ -10,14 +10,14 @@
 
 /*selesai*/
 // #define modelJ505
-#define modelJ506
+// #define modelJ506
 // #define modelJ508
 // #define modelJ507
-
-// #define modelL0064
-// #define modelL0512
-// #define modelL0128
+// #define modelJ503
 // #define modelJ504
+// #define modelJ501
+
+#define modelJ502 // MODUL NO NAME
 
 #define read_little_end
 #include <Arduino.h>
@@ -40,10 +40,10 @@
     (defined(modelJ506) ? 1 : 0) +  \
     (defined(modelJ504) ? 1 : 0) +  \
     (defined(modelJ505) ? 1 : 0) +  \
-    (defined(modelL0064) ? 1 : 0) + \
-    (defined(modelL0128) ? 1 : 0) + \
+    (defined(modelJ503) ? 1 : 0) + \
+    (defined(modelJ501) ? 1 : 0) + \
     (defined(modelJ507) ? 1 : 0) +  \
-    (defined(modelL0512) ? 1 : 0))
+    (defined(modelJ502) ? 1 : 0))
 
 #if COUNT_MODELS == 0
 #error "Error: DEFINE MODEL SEK SUU!"
@@ -55,7 +55,7 @@
 const int pin_IO[sizeRelay] = {29, 33, 43, 25, 34, 44, 28, 63, 49, 38, 62, 40, 24, 39, 46, 64, 31, 37, 35, 23, 41, 48, 22, 45, 32, 30, 26, 65, 42, 47, 27, 36};
 #elif defined(modelJ507)
 const int pin_IO[sizeRelay] = {26, 29, 22, 23, 41, 47, 31, 45, 63, 25, 34, 64, 49, 65, 28, 40, 27, 33, 48, 30, 46, 42, 35, 39, 44, 43, 62, 37, 38, 36, 24, 32};
-#elif defined(modelL0512)
+#elif defined(modelJ502)
 const int pin_IO[sizeRelay] = {35, 22, 65, 26, 44, 45, 62, 48, 31, 38, 37, 23, 33, 39, 28, 43, 42, 29, 25, 40, 32, 64, 30, 46, 34, 27, 49, 41, 24, 47, 36, 63};
 #elif defined(modelJ504)
 const int pin_IO[sizeRelay] = {47, 41, 23, 38, 46, 31, 35, 27, 64, 43, 44, 33, 29, 49, 37, 25, 26, 34, 24, 63, 28, 30, 32, 22, 39, 45, 65, 40, 48, 42, 62, 36};
@@ -64,9 +64,9 @@ const int pin_IO[sizeRelay] = {37, 36, 24, 33, 48, 26, 62, 44, 41, 47, 38, 32, 2
 
 #elif defined(modelJ506)
 const int pin_IO[sizeRelay] = {29, 28, 26, 30, 32, 27, 65, 43, 22, 47, 41, 42, 39, 45, 34, 40, 62, 24, 31, 25, 48, 33, 49, 38, 37, 36, 35, 64, 23, 44, 46, 63};
-#elif defined(modelL0064)
+#elif defined(modelJ503)
 const int pin_IO[sizeRelay] = {36, 65, 44, 49, 23, 37, 30, 24, 31, 22, 35, 25, 43, 26, 28, 45, 62, 39, 42, 38, 47, 48, 64, 41, 63, 33, 46, 32, 40, 34, 29, 27};
-#elif defined(modelL0128)
+#elif defined(modelJ501)
 const int pin_IO[sizeRelay] = {49, 39, 29, 35, 37, 25, 30, 44, 26, 42, 63, 48, 41, 22, 43, 28, 23, 62, 45, 27, 65, 46, 32, 34, 36, 24, 64, 47, 38, 31, 40, 33};
 #endif
 // 50 -> 62
@@ -254,18 +254,18 @@ private:
 #elif defined(modelJ505)
     byte eth_mac[S_MAC] = {0x02, 0xA1, 0x01, 0x16, 0x3D, 0x5F};
     const char *controller_name = "J505";
-#elif defined(modelL0064)
-    const char *controller_name = "L0064";
+#elif defined(modelJ503)
+    const char *controller_name = "J503";
     byte eth_mac[S_MAC] = {0x02, 0xA1, 0x01, 0x16, 0x3D, 0x60};
-#elif defined(modelL0128)
+#elif defined(modelJ501)
     byte eth_mac[S_MAC] = {0x02, 0xA1, 0x01, 0x16, 0x3D, 0x61};
-    const char *controller_name = "L0128";
+    const char *controller_name = "J501";
 #elif defined(modelJ507)
     byte eth_mac[S_MAC] = {0x02, 0xA1, 0x01, 0x16, 0x3D, 0x62};
     const char *controller_name = "J507";
-#elif defined(modelL0512)
+#elif defined(modelJ502)
     byte eth_mac[S_MAC] = {0x02, 0xA1, 0x01, 0x16, 0x3D, 0x63};
-    const char *controller_name = "L0512";
+    const char *controller_name = "J502";
 #endif
 
     char device_class[32] = "not_set";
